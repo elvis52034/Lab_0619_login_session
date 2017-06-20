b965ad78676ccc6a616019bbdc4b522ef191764d
elvis52034 <elvis52034@gmail.com>
2017-06-20 09:02:33 +0800

    Add files via upload

---
+++ Lab_LoginSession/Login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        if (string.IsNullOrEmpty(txtUserName.Text))
+        {
+            lblPrompt.Text = "請輸入你的帳號";
+            return;
+        }
+
+        Session["uid"]= txtUserName.Text;
+
+        string c = Session["uid"].ToString();
+        if (c == null)
+            Response.Redirect("Default.aspx");
+        else
+            Response.Redirect("Secret.aspx");
+
+
+    }
+}
---
+++ Lab_LoginSession/Site.master.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SiteMaster : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string b = Session["uid"].ToString();
+        if (b != null) {
+            string log = "Logout";
+        } else { 
+            string log = "Login";
+        }
+    }
+}
