0f2d2aa06a755485580ab977341dae60b7fb9406
elvis52034 <elvis52034@gmail.com>
2017-06-21 15:23:27 +0800

    Add files via upload
    修改主板login及logout標示

---
+++ Lab_LoginSession/Lgout.aspx
@@ -0,0 +1,13 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true" CodeFile="Lgout.aspx.cs" Inherits="Lgout" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" Runat="Server">
+
+    
+    <div>
+    <h1>你已登出</h1>
+    </div>
+
+
+
+</asp:Content>
--- Lab_LoginSession/Site.master.cs
+++ Lab_LoginSession/Site.master.cs
@@ -5,15 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class SiteMaster : System.Web.UI.MasterPage
-{
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        string b = Session["uid"].ToString();
-        if (b != null) {
-            string log = "Logout";
-        } else { 
-            string log = "Login";
+
+    public partial class SiteMaster : System.Web.UI.MasterPage {
+        string log = "";
+        protected void Page_Load(object sender, EventArgs e) {
+           
         }
     }
-}
+
